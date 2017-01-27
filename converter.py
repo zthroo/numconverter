6d9e9f20b6fc8be4764b14045964eebab88de011
zthroo <pgoldenstein@gmail.com>
2017-01-27 14:18:21 -0600

    added in argument parsing

--- converter.py
+++ converter.py
@@ -1,7 +1,8 @@
 import math
+import argparse
 
 
-def bin2dec(length, num_str):       #converts binary to decimal
+def bin2dec(length, num_str):  # converts binary to decimal
     num = 0
     for index in range(length):
         num = num + (2 ** (length - index - 1)) * int(num_str[index])
@@ -10,20 +11,20 @@ def bin2dec(length, num_str):       #converts binary to decimal
 
 # converts binary to hexadecimal
 def bin2hex(length, num_string):
-    offset = 4 - length % 4             #determine how many 0s needed to make the string length evenly divisible by 4
+    offset = 4 - length % 4  # determine how many 0s needed to make the string length evenly divisible by 4
     if length < 4:
-        for i in range(4 - length):         #add 0s
+        for i in range(4 - length):  # add 0s
             num_string = "0" + num_string
     else:
-        for i in range(offset):             #add 0s
+        for i in range(offset):  # add 0s
             num_string = "0" + num_string
     num_list = []
-    for i in range((math.floor(len(num_string) / 4))):      #convert string into list of 4 bit nibble
+    for i in range((math.floor(len(num_string) / 4))):  # convert string into list of 4 bit nibble
         num_list.append(num_string[i * 4] + num_string[i * 4 + 1] + num_string[i * 4 + 2] + num_string[i * 4 + 3])
-    #print(num_list)
-    for i in range(len(num_list)):      #convert cells to decimal numbers
+    # print(num_list)
+    for i in range(len(num_list)):  # convert cells to decimal numbers
         num_list[i] = bin2dec(4, num_list[i])
-    for i in range(len(num_list)):      #convert decimals to hex representation
+    for i in range(len(num_list)):  # convert decimals to hex representation
         if num_list[i] == 10:
             num_list[i] = "A"
         if num_list[i] == 11:
@@ -38,41 +39,74 @@ def bin2hex(length, num_string):
             num_list[i] = "F"
 
     ans_string = "0x"
-    for cell in num_list:           #runs through num_list, adding the hex characters to the answer string
+    for cell in num_list:  # runs through num_list, adding the hex characters to the answer string
         ans_string = ans_string + str(cell)
     return ans_string
 
 
 # converts hexadecimal to binary
-def hex2bin(num_str):
-    list(num_str)               #convert num_string to list
-    num_str = num_str[2:]       #pop off the 0x
-    #dict to convert between the hex char and the binary string
-    hex_dict = {'0':"0000", '1':"0001", '2':"0010", '3':"0011", '4':"0100", '5':"0101", '6':"0110",
-                '7':"0111", '8':"1000", '9':"1001", 'A':"1010", 'B':"1011", 'C':"1100", 'D':"1101",
-                'E':"1110", 'F':"1111"}
+def hex2bin(length, num_str):
+    list(num_str)  # convert num_string to list
+    num_str = num_str[2:]  # pop off the 0x
+    # dict to convert between the hex char and the binary string
+    hex_dict = {'0': "0000", '1': "0001", '2': "0010", '3': "0011", '4': "0100", '5': "0101", '6': "0110",
+                '7': "0111", '8': "1000", '9': "1001", 'A': "1010", 'B': "1011", 'C': "1100", 'D': "1101",
+                'E': "1110", 'F': "1111"}
     ans_string = ""
-    for letter in num_str:      #runs through the num_string, adding the nibbles to the ans_string
+    for letter in num_str:  # runs through the num_string, adding the nibbles to the ans_string
         ans_string = ans_string + hex_dict[letter]
     return ans_string
 
 
 # converts decimal to binary
 def dec2bin(num):
-    ans = ""                            #empty answer string
-    while num > 0:                      #while loop to produce the binary string
-        temp = num % 2                  #find remainder of division by 2
-        ans = str(temp) + ans           #add remainder to the beginning of ans
-        num = math.floor(num / 2)       #get next number for pass through loop
+    ans = ""  # empty answer string
+    while num > 0:  # while loop to produce the binary string
+        temp = num % 2  # find remainder of division by 2
+        ans = str(temp) + ans  # add remainder to the beginning of ans
+        num = math.floor(num / 2)  # get next number for pass through loop
         print(ans)
     return ans
 
 
 # converts decimal to hexadecimal
 def dec2hex(num):
-    temp = dec2bin(num)                 #converts input to binary
-    length = len(temp)                  #gets length of binary
-    return bin2hex(length, temp)        #converts binary to hex and returns
+    temp = dec2bin(num)  # converts input to binary
+    length = len(temp)  # gets length of binary
+    return bin2hex(length, temp)  # converts binary to hex and returns
 
 
-print(dec2hex(156))
+def main():
+    parser = argparse.ArgumentParser()  # create argument parser
+    parser.add_argument("-ct", "--conversion_type",
+                        help="type of conversion to perform")  # adds conversion type argument
+    parser.add_argument("-fi", "--file_in_name", help="name of the file to take in")  # adds file in name argument
+    parser.add_argument("-fo", "--file_out_name", help="name of the file to produce")  # adds file out name  argument
+    args = parser.parse_args()  # parse the arguments
+    fin = open(args.fi, 'r')  # creates file in object
+    fout = open(args.fo, 'w')  # creates file out object
+    num_nums = fin.readline()
+    print(num_nums)
+
+    if args.ct == "bin2dec":  # read in file, process each line using bin2dec, write to output file
+        for line in fin:
+            print(line, end='')
+        return
+
+    if args.ct == "bin2hex":  # read in file, process each line using bin2hex, write to output file
+        for line in fin:
+            print(line, end='')
+        return
+
+    if args.ct == "dec2bin":  # read in file, process each line using dec2bin, write to output file
+        for line in fin:
+            print(line, end='')
+        return
+
+    if args.ct == "dec2hex":  # read in file, process each line using dec2hex, write to output file
+        for line in fin:
+            print(line, end='')
+        return
+
+if __name__ == '__main__':
+    main()
